58e6eb49512cdbc64b5d775582789d4e0dcb146f
emilyploszaj <emilyploszaj@gmail.com>
2020-12-13 22:49:53 -0600

    Day 13 p1 update

--- d13/p1/source/app.d
+++ d13/p1/source/app.d
@@ -4,21 +4,19 @@ import std.file;
 import std.stdio;
 import std.string;
 
+// I started part 2 here and moved then lost my undo history so this doesn't work lol
 void main() {
 	string input = cast(string) read("../input.txt");
 	string[] lines = input.split("\n")[0..$ - 1];
-	//int time = lines[0].to!int;
+	int time = lines[0].to!int;
 	string[] ids = lines[1].split(",");
 	int wait = 9999999;
 	int wId;
-	outer: 
 	for (int t = 0; ; t++) {
 		for (int i = 0; i < ids.length; i++) {
 			if (ids == 'x') continue;
 			int id = ids.to!int;
 		}
 	}
-	writeln(wait);
-	writeln(wId);
 	writeln(wait * wId);
 }
